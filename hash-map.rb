d66f1e6eb8fd8e72b1e736c060dd65b545603808
Yoni190 <yfisuhu@gmail.com>
2024-09-06 13:50:34 +0300

    Create a method to set a key-value pair in a bucket
    The method does the following:
    - Produces a hash code for the key
    - The modulus of the hash code is used as index
    - The key-value pair is then assigned in the bucket

---
+++ hash-map.rb
@@ -0,0 +1,17 @@
+class HashMap
+  @@bucket = Array.new(16)
+
+  def hash(key)
+    hash_code = 0
+    prime_number = 31
+
+    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+    hash_code
+  end
+
+  def set(key, value)
+    hash_code = hash(key)
+    index = hash_code % 16
+    @@bucket[index] = {key => value}
+  end
+end
